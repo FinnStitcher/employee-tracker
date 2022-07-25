912d4047ea3112c8b8693ffec9a8e893f8afeb82
Finn Stitcher <foundfootage@protonmail.com>
2022-07-25 15:39:25 -0700

    loading lists into prompt

--- app.js
+++ app.js
@@ -1,99 +1,199 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
-const db = require('./db/connection');
-const sql = require('./db/sql.js');
-const getListItems = require('./utils/getListItems');
+const db = require("./db/connection");
+const sql = require("./db/sql.js");
 
 const dummyData = {
-    employeeInvalidManager: ['Sarah', 'Page', 9, 99],
+	employeeInvalidManager: ["Sarah", "Page", 9, 99],
 };
 // i'll come back to the manager test
+
+db.promise().query(sql.viewAllNames).then(([rows]) => {
+    let deptList = rows.map(element => element.name);
+    let roleList = rows.map(element => element.title);
+    let employeeList = rows.map(element => `${element.first_name} ${element.last_name}`);
+
+    return {
+        deptList,
+        roleList,
+        employeeList
+    };
+})
+.then(lists => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'select',
+            message: 'What would you like to do?',
+            choices: [
+                'Add Employee',
+                'View All Employees',
+                'Update Employee Role',
+                'Add Role',
+                'View All Roles',
+                'Add Department',
+                'View All Departments'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'deptName',
+            message: 'What is the name of the department?',
+            when: prev => prev.select === 'Add Department'
+        },
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?',
+            when: prev => prev.select === 'Add Role'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary of the role?',
+            when: prev => prev.select === 'Add Role'
+        },
+        {
+            type: 'list',
+            name: 'roleDept',
+            message: 'What department does the role belong to?',
+            choices: [...lists.deptList],
+            when: prev => prev.select === 'Add Role'
+        },
+        {
+            type: 'input',
+            name: 'employeeForename',
+            message: 'What is the employee\'s first name?',
+            when: prev => prev.select === 'Add Employee'
+        },
+        {
+            type: 'input',
+            name: 'employeeSurname',
+            message: 'What is the employee\'s last name?',
+            when: prev => prev.select === 'Add Employee'
+        },
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What is the employee\'s role?',
+            choices: [...lists.roleList],
+            when: prev => prev.select === 'Add Employee'
+        },
+        {
+            type: 'list',
+            name: 'employeeManager',
+            message: 'Who is the employee\'s manager?',
+            choices: [...lists.employeeList],
+            when: prev => prev.select === 'Add Employee'
+        },
+        {
+            type: 'list',
+            name: 'updateRoleName',
+            message: 'Which employee do you want to update?',
+            choices: [...lists.employeeList],
+            when: prev => prev.select === 'Update Employee Role'
+        },
+        {
+            type: 'list',
+            name: 'updateRoleRole',
+            message: 'What should their role be updated to?',
+            choices: [...lists.roleList],
+            when: prev => prev.select === 'Update Employee Role'
+        }
+    ])
+    .then(results => {
+        console.log(results)
+    });
+});
 
 // i'm thinking when the application begins, it'll initialize a list of departments, roles, and possible managers based on what's already in the database?
 // oh, but i would have to make it possible for that to run repeatedly as necessary
 // so it'll be in a function
 
-inquirer.prompt([
-    {
-        type: 'list',
-        name: 'select',
-        messsage: 'What would you like to do?',
-        choices: [
-            'Add Employee',
-            'View All Employees',
-            'Update Employee Role',
-            'Add Role',
-            'View All Roles',
-            'Add Department',
-            'View All Departments'
-        ]
-    },
-    {
-        type: 'input',
-        name: 'deptName',
-        message: 'What is the name of the department?',
-        when: prev => prev.select === 'Add Department'
-    },
-    {
-        type: 'input',
-        name: 'roleName',
-        message: 'What is the name of the role?',
-        when: prev => prev.select === 'Add Role'
-    },
-    {
-        type: 'input',
-        name: 'roleSalary',
-        message: 'What is the salary of the role?',
-        when: prev => prev.select === 'Add Role'
-    },
-    {
-        type: 'input', // temp
-        name: 'roleDept',
-        message: 'What department does the role belong to?',
-        // could have a choices option here that's filled with the return of a function?
-        when: prev => prev.select === 'Add Role'
-    },
-    {
-        type: 'input',
-        name: 'employeeForename',
-        message: 'What is the employee\'s first name?',
-        when: prev => prev.select === 'Add Employee'
-    },
-    {
-        type: 'input',
-        name: 'employeeSurname',
-        message: 'What is the employee\'s last name?',
-        when: prev => prev.select === 'Add Employee'
-    },
-    {
-        type: 'input', //temp
-        name: 'employeeRole',
-        message: 'What is the employee\'s role?',
-        when: prev => prev.select === 'Add Employee'
-    },
-    {
-        type: 'list',
-        name: 'employeeManager',
-        message: 'Who is the employee\'s manager?',
-        choices: ['dummy text'],
-        when: prev => prev.select === 'Add Employee'
-    },
-    {
-        type: 'input', //temp
-        name: 'updateRoleName',
-        message: 'Which employee do you want to update?',
-        when: prev => prev.select === 'Update Employee Role'
-    },
-    {
-        type: 'input', //temp
-        name: 'updateRoleRole',
-        message: 'What should their role be updated to?',
-        when: prev => prev.select === 'Update Employee Role'
-    }
-])
-.then(results => {
-    
-})
+// inquirer.prompt([
+//     {
+//         type: 'list',
+//         name: 'select',
+//         message: 'What would you like to do?',
+//         choices: [
+//             'Add Employee',
+//             'View All Employees',
+//             'Update Employee Role',
+//             'Add Role',
+//             'View All Roles',
+//             'Add Department',
+//             'View All Departments'
+//         ]
+//     },
+//     {
+//         type: 'input',
+//         name: 'deptName',
+//         message: 'What is the name of the department?',
+//         when: prev => prev.select === 'Add Department'
+//     },
+//     {
+//         type: 'input',
+//         name: 'roleName',
+//         message: 'What is the name of the role?',
+//         when: prev => prev.select === 'Add Role'
+//     },
+//     {
+//         type: 'input',
+//         name: 'roleSalary',
+//         message: 'What is the salary of the role?',
+//         when: prev => prev.select === 'Add Role'
+//     },
+//     {
+//         type: 'list',
+//         name: 'roleDept',
+//         message: 'What department does the role belong to?',
+//         choices: [...deptList],
+//         when: prev => prev.select === 'Add Role'
+//     },
+//     {
+//         type: 'input',
+//         name: 'employeeForename',
+//         message: 'What is the employee\'s first name?',
+//         when: prev => prev.select === 'Add Employee'
+//     },
+//     {
+//         type: 'input',
+//         name: 'employeeSurname',
+//         message: 'What is the employee\'s last name?',
+//         when: prev => prev.select === 'Add Employee'
+//     },
+//     {
+//         type: 'list',
+//         name: 'employeeRole',
+//         message: 'What is the employee\'s role?',
+//         choices: [...roleList],
+//         when: prev => prev.select === 'Add Employee'
+//     },
+//     {
+//         type: 'list',
+//         name: 'employeeManager',
+//         message: 'Who is the employee\'s manager?',
+//         choices: [...employeeList],
+//         when: prev => prev.select === 'Add Employee'
+//     },
+//     {
+//         type: 'list',
+//         name: 'updateRoleName',
+//         message: 'Which employee do you want to update?',
+//         choices: [...employeeList],
+//         when: prev => prev.select === 'Update Employee Role'
+//     },
+//     {
+//         type: 'list',
+//         name: 'updateRoleRole',
+//         message: 'What should their role be updated to?',
+//         choices: [...roleList],
+//         when: prev => prev.select === 'Update Employee Role'
+//     }
+// ])
+// .then(results => {
+//     console.log(results)
+// });
 // .then(results => {
 //     switch (results.select) {
 //         case 'Add Employee': {
--- db/sql.js
+++ db/sql.js
@@ -1,36 +1,19 @@
-// departments
-const viewDept = `SELECT * FROM department`;
-
-const addDept = `INSERT INTO department (name) VALUES (?)`;
-
-// roles
-const viewRole = `SELECT role.id, role.title, department.name AS department, salary
+module.exports = {
+	viewAllNames: `SELECT department.name, role.title, employee.first_name, employee.last_name
+FROM department
+JOIN role ON department.id = role.department_id
+JOIN employee ON role.id = employee.role_id`,
+	viewDept: `SELECT * FROM department`,
+	addDept: `INSERT INTO department (name) VALUES (?)`,
+	viewRole: `SELECT role.id, role.title, department.name AS department, salary
 FROM role
-JOIN department ON department.id = role.department_id`;
-
-const addRole = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-
-// employees
-const viewEmployee = `SELECT employee.id, employee.first_name, employee.last_name,
+JOIN department ON department.id = role.department_id`,
+	addRole: `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+	viewEmployee: `SELECT employee.id, employee.first_name, employee.last_name,
 role.title, department.name AS department, role.salary
 FROM employee
 JOIN role ON role.id = employee.role_id
-JOIN department ON department.id = role.department_id`;
-// going to try adding managers with javascript on the outside, because the demo shows their full names in one column
-
-const addEmployee = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`
-
-const updateEmployeeRole = `UPDATE employee SET role_id = ? WHERE id = ?`;
-
-// probably best to do data validation before the query goes to the database
-// we'll do that later as part of inquirer
-
-module.exports = {
-    viewDept,
-    addDept,
-    viewRole,
-    addRole,
-    viewEmployee,
-    addEmployee,
-    updateEmployeeRole
+JOIN department ON department.id = role.department_id`,
+	addEmployee: `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+	updateEmployeeRole: `UPDATE employee SET role_id = ? WHERE id = ?`,
 };
