f7ed82df884b9a5a779e2ef2bdd15435d90c3cac
Finn Stitcher <foundfootage@protonmail.com>
2022-07-30 13:57:25 -0700

    printing managers

--- db/sql.js
+++ db/sql.js
@@ -6,10 +6,11 @@ module.exports = {
 FROM role
 JOIN department ON department.id = role.department_id`,
 	addRole: `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
-	viewEmployee: `SELECT employee.id, employee.first_name, employee.last_name,
-role.title, department.name AS department, role.salary
-FROM employee
-JOIN role ON role.id = employee.role_id
+	viewEmployee: `SELECT e1.id, e1.first_name, e1.last_name,
+role.title, department.name AS department, role.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager
+FROM employee e1
+JOIN employee e2 ON e2.id = e1.manager_id
+JOIN role ON role.id = e1.role_id
 JOIN department ON department.id = role.department_id`,
 	addEmployee: `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
 	updateEmployeeRole: `UPDATE employee SET role_id = ? WHERE id = ?`,
